7f0475bef918ed2a65a71a19f61fc1a48f8ac003
Leonardo Fernandes <leonardobarroso@edu.unifor.br>
2021-06-07 00:39:41 -0300

    Working with four verbs

---
+++ 02_RestASPnet5_Calculator/RestASPnet5/RestASPnet5/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestASPnet5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+  
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondeNumber}")]
+        public IActionResult Sum(string firstNumber, string secondeNumber)
+        {
+ 
+            return BadRequest("Invalide Input");
+        }
+    }
+}
